da93e78305b326b825db49e01cc55e728729a3c0
leibo.sun@ptmind.com <leibo.sun@ptmind.com>
2017-10-17 21:33:34 +0800

    提交点在多边形内的helper函数

--- src/sd3/helper.js
+++ src/sd3/helper.js
@@ -5,9 +5,9 @@ export default {
 
         var path = d3.path();
         path.moveTo(startPos.x, startPos.y);
-        path.lineTo(startPos.x + (endPos.x-startPos.x)/2 , startPos.y);
-        path.lineTo(startPos.x + (endPos.x-startPos.x)/2  , endPos.y);
-        path.lineTo(endPos.x , endPos.y-3)
+        path.lineTo(startPos.x + (endPos.x - startPos.x) / 2, startPos.y);
+        path.lineTo(startPos.x + (endPos.x - startPos.x) / 2, endPos.y);
+        path.lineTo(endPos.x, endPos.y - 3)
         // path.closePath();
         // console.log(path.toString())
         var str = path.toString().replace(/,/g, ' ').replace(/([A-Z])/g, ' $1');
@@ -22,8 +22,27 @@ export default {
         //     .attr("d", aa);
 
         return aa;
-
-
+    },
+    intersects(circle, polygon) {
+        return this.pointInPolygon(circle, polygon)
+            || this.polygonEdges(polygon).some(function (line) { return this.pointLineSegmentDistance(circle, line) < circle[2]; });
+    },
+    pointInPolygon(point, polygon) {
+        for (var n = polygon.length, i = 0, j = n - 1, x = point[0], y = point[1], inside = false; i < n; j = i++) {
+            var xi = polygon[i][0], yi = polygon[i][1],
+                xj = polygon[j][0], yj = polygon[j][1];
+            if ((yi > y ^ yj > y) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi)) inside = !inside;
+        }
+        return inside;
+    },
+    pointPointSquaredDistance(v, w) {
+        var dx = v[0] - w[0], dy = v[1] - w[1];
+        return dx * dx + dy * dy;
+    },
+    polygonEdges(polygon) {
+        return polygon.map(function (p, i) {
+            return i ? [polygon[i - 1], p] : [polygon[polygon.length - 1], p];
+        });
     }
 
 }
